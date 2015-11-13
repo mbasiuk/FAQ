73594ac2a1b67b17524729d41d174ae38407cb11
mykhailo basiuk <wacok.mail.ru@gmail.com>
2015-11-13 15:48:30 +0200

    disable lags on keydowns

--- Faq.Wpf/MainWindow.xaml.cs
+++ Faq.Wpf/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System;
 using System.Linq;
 using Microsoft.Expression.Interactivity.Core;
+using System.Windows.Input;
 
 namespace Test
 {
@@ -32,7 +33,7 @@ namespace Test
             else
             {
                 DataContext = Faq.Library.Faq.GetAllFaq();
-                Observable.FromEventPattern<TextChangedEventArgs>(searchText, "TextChanged")                    
+                Observable.FromEventPattern<TextChangedEventArgs>(searchText, "TextChanged")
                     .Throttle(TimeSpan.FromMilliseconds(200))
                     .ObserveOn(System.Threading.SynchronizationContext.Current)
                     .Subscribe(messages => Hande(messages));
@@ -46,28 +47,16 @@ namespace Test
                 listBox.ItemsSource = Faq.Library.Faq.GetAllFaq();
             }
             else
-            {                
+            {
                 listBox.ItemsSource = Faq.Library.Faq.FindFaq(string.Format("%{0}%", searchText.Text));
             }
-            return messages;
-        }
 
-        public void FocusTextBoxSearch()
-        {
-            searchText.Focus();
+            return messages;
         }
 
         private void ScrollViewer_ManipulationBoundaryFeedback(object sender, System.Windows.Input.ManipulationBoundaryFeedbackEventArgs e)
         {
             e.Handled = true;
-        }
-
-        private void searchText_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if(e.Key == System.Windows.Input.Key.Escape)
-            {
-                e.Handled = true;
-            }
         }
     }
 }
